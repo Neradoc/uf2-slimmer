f85519dcdd44dc44497ee46075cf422512cd218b
Neradoc <neraOnGit@ri1.fr>
2024-12-04 23:25:11 +0100

    git ignore et update

--- uf2_slimmer.py
+++ uf2_slimmer.py
@@ -8,8 +8,17 @@ import sys
 import click
 from pathlib import Path
 
-def slim_files(file_in, file_out):
+BLOCK_ADDR = 12
+PAYLOAD_SIZE = 16
+BLOCK_IDX = 20
+BLOCK_NUM = 24
+PAYLOAD = 32
+
+
+def slim_files(file_in, file_out, blocknum=True):
     data_out = []
+    bnum = 0
+    addr0 = 0
 
     with open(file_in, "rb") as finput:
         num = 0
@@ -18,28 +27,55 @@ def slim_files(file_in, file_out):
             magic = data[0:4]
             if magic != b"UF2\n":
                 print(num, "bad magic", magic)
-            payload_size = int.from_bytes(data[16:20], "little")
+                sys.exit(2)
+            payload_size = int.from_bytes(
+                data[PAYLOAD_SIZE : PAYLOAD_SIZE + 4], "little"
+            )
             # print(num, payload_size)
-            payload = data[32: 32 + payload_size]
+            payload = data[PAYLOAD : PAYLOAD + payload_size]
             do_write = False
             for byte in payload:
                 if byte != 0xFF:
                     do_write = True
                     break
+
+            addr = int.from_bytes(data[BLOCK_ADDR : BLOCK_ADDR + 4], "little")
+            if addr0 > 0 and addr != addr0 + 0x100:
+                print(f"addr {addr0} {addr}")
+            addr0 = addr
+
+            bbnum = int.from_bytes(data[BLOCK_IDX : BLOCK_IDX + 4], "little")
+            if bbnum > 0 and bbnum != bnum + 1:
+                print(bnum, bbnum)
+            bnum = bbnum
+
             if do_write:
-                data[20:24] = (block_index).to_bytes(4, "little")
+                if blocknum:
+                    data[BLOCK_IDX : BLOCK_IDX + 4] = (block_index).to_bytes(
+                        4, "little"
+                    )
                 data_out.append(data)
+                if len(data) != 512:
+                    raise ValueError("WTF")
                 block_index += 1
-                # foutput.write(data)
             num += 1
 
-    # 
+    import binascii
+
+    num_blocks_bytes = (block_index).to_bytes(4, "little")
     print(f"Blocks: {block_index}/{num} Size: {256 * block_index:,} B")
+    # print(binascii.hexlify(num_blocks_bytes))
 
     with open(file_out, "wb") as foutput:
         for data in data_out:
-            data[24:28] = (block_index).to_bytes(4, "little")
-            foutput.write(data)
+            if blocknum:
+                data[BLOCK_NUM : BLOCK_NUM + 4] = num_blocks_bytes
+            if len(data) != 512:
+                raise ValueError("WTF")
+            num = foutput.write(data)
+            if num != 512:
+                raise ValueError("WTF")
+
 
 @click.command()
 @click.argument(
@@ -51,26 +87,37 @@ def slim_files(file_in, file_out):
     required=False,
 )
 @click.option(
-    "--force", "-f",
+    "--force",
+    "-f",
     is_flag=True,
-    help="Overwrite output file if it exists."
+    help="Overwrite output file if it exists.",
 )
-def main(file_in, file_out, force):
+@click.option(
+    "--blocknum/--noblocknum",
+    "-b/-B",
+    is_flag=True,
+    default=None,
+    help="Write new block number values (default true).",
+)
+def main(file_in, file_out, force, blocknum):
     """
     Slim down a UF2 file by removing the blocks that are all FF.
     This assumes that it will be flashed on a previously erased or brand new flash chip.
 
     The output file will be automatically named by appending ".slim" to the name.
     """
+    if blocknum is None:
+        blocknum = True
     if file_out is None:
         file_out = file_in.rstrip(".uf2") + ".slim.uf2"
     if Path(file_out).exists() and not force:
         click.secho(
             f"ERROR: Output file {file_out} already exists !\n"
             "Use -f to overwrite anyway.",
-            fg="red"
+            fg="red",
         )
         sys.exit(1)
-    slim_files(file_in, file_out)
+    slim_files(file_in, file_out, blocknum)
+
 
 main()
